1ec1faff42884b43379208b243d0b9232fd4e577
konrad <dlahuawei@gmail.com>
2012-05-29 08:58:35 +0200

    6

--- config/routes.rb
+++ config/routes.rb
@@ -58,7 +58,7 @@ Devise2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'books#index'
+  root :to => 'events#index'
 
   # See how all your routes lay out with "rake routes"
 
